a455488a3057522199b27eba830959aac981555e
mert <>
2022-09-23 18:52:09 +0300

    redux toolkit with typescript

---
+++ src/store/store.ts
@@ -0,0 +1,17 @@
+import {configureStore} from "@reduxjs/toolkit";
+import {useDispatch, useSelector, TypedUseSelectorHook} from "react-redux";
+import todoSlice from "../features/todoSlice";
+
+const store = configureStore({
+    reducer: {
+        todos: todoSlice,
+    },
+});
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch;
+
+export const UseAppDispatch = () => useDispatch<AppDispatch>();
+export const UseAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export default store;
